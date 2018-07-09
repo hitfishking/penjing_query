a1e9707e219074de44fcd7715e15086989de76c5
hitfishking <1063888400@qq.com>
2018-07-09 13:09:35 +0800

    重新规划urls。

--- pjquery/urls.py
+++ pjquery/urls.py
@@ -4,17 +4,20 @@
 # 尽量局部化，模块独立化，再全局引用，是django框架模块化思想的重要运用；
 # ------------------------------------------
 from django.conf.urls import url
-from . import views, views2
+from . import views
+from . import views2
 from pjquery.views2 import Magazine_Controller
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^old/', views.index_old, name='index_old'),
+    url(r'^0/$', views.index_0, name='index_0'),
+    url(r'^1/$', views.index_1, name='index_1'),
+    url(r'^2/$', views.index_2, name='index_2'),
+    url(r'^$', views.index_2, name='index'),     # 默认显示bootstrap-table插件版主页；
 
+    url(r'^1/search1/$', views.search_1, name='search_1'),
+    url(r'^2/search2/$', views.search_2, name='search_2'),
+
+    # ------------------------类controller版功能---------------
     url(r'^query1/', Magazine_Controller.as_view(), name='query_dir1'),
-    url(r'^query2/', views2.query_dir2, name='query_dir2'),
-
-    url(r'^search/', views.search_keywords, name='search_keywords'),
-    url(r'^search2/', views.search_keywords2, name='search_keywords2'),
-
+    url(r'^query2/', views2.query_dir, name='query_dir2'),
 ]
--- pjquery/views.py
+++ pjquery/views.py
@@ -6,20 +6,31 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Dir, Magazine
 from django.http import JsonResponse
-import json
 
-# 功能：默认主页; 显示某一年的杂志目录；或空白页；
-# url：""
+# 最早版本模板，没有任何样式，只显示原始数据。
+# url：'^0/'
+def index_0(request):
+    return render(request, 'pjquery/index_0.html')  # 纯一次性显示，没有页内交互；
+
+
+# 功能：boostrap标准table版主页;
+# url：'^1/'
 # 参数: Magazine.object(***)
-def index(request):
-    return render(request, 'pjquery/index2.html', {'magazines': [] })
+def index_1(request):
+    return render(request, 'pjquery/index_1.html', {'magazines': [] })
 
 
-# 功能：在search bar上输入关键字，对杂志文章目录进行搜索；
-# url：^search/
+# 功能：boostrap-table插件版主页;
+# url：'^2/'
+def index_2(request):
+    return render(request, 'pjquery/index_2.html')
+
+
+# 功能：bootstrap标准table版search；在search bar上输入关键字，对杂志文章目录进行搜索；
+# url：^1/search1/
 # 输入：inputwords：string；
 # 输出：查询结果信息二维数组[[year, month, article_name, article_page],...]
-def search_keywords(request):
+def search_1(request):
     inputwords = str.strip(request.POST['inputwords'])
     if inputwords == "":
         return redirect('index')
@@ -33,16 +44,16 @@ def search_keywords(request):
             if inputwords in j.article_name:
                 resultSet.append([i.year, i.month, j.article_name, j.article_page])
 
-        return render(request, 'pjquery/index.html', {'results': resultSet})
+        return render(request, 'pjquery/index_1.html', {'results': resultSet})
 
 
 # 功能：在search bar上输入关键字，对杂志文章目录进行搜索；
-#      本action用于响应返回bootstrap-table的template(index2.html)；
-# url：^search2/
+#      本action用于响应返回bootstrap-table的template(index_2.html)；
+# url：^2/search2/
 # 输入：inputwords：string；
 # 输出：查询结果信息二维数组[[year, month, article_name, article_page],...]
 #      以json格式返回;
-def search_keywords2(request):
+def search_2(request):
     inputwords = str.strip(request.GET['inputwords'])
     if inputwords == "":
         pass
@@ -69,17 +80,3 @@ def search_keywords2(request):
 
         return JsonResponse(data)
 
-
-def index_old(request):
-    return render(request, 'pjquery/index_old.html')  # 每个应用app，模板都默认存储在该app的templates子目录下；
-    # return HttpResponse("hello world！")
-
-
-def query_dir(request):
-    arr1=[]
-    for i in Magazine.objects(year='2011',month='1'):
-        for j in i.dir:
-            arr1.append(j.article_name)
-            arr1.append('<br/>')
-
-    return HttpResponse(arr1)
